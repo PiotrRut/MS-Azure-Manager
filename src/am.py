ac6e52e22b2c45c371a10e5f6f14782889a79a80
PiotrRut <piotr.rutkowski@city.ac.uk>
2020-12-15 18:24:37 +0100

    Add download blob func + some changes :sparkles:

--- src/am.py
+++ src/am.py
@@ -4,6 +4,8 @@ from helpers import show_help
 
 from azure.storage.blob import BlobServiceClient, ContentSettings, ContainerClient
 
+blob_service_client = BlobServiceClient.from_connection_string(os.getenv('AZURE_CON_STR'))
+
 
 # Decide which function to run based on user input
 def decider():
@@ -27,12 +29,19 @@ def decider():
                 print("Starting upload")
                 upload_all()
                 print(f"All files successfully uploaded to {sys.argv[2]} container")
-        elif sys.argv[1] == 'showall':
+        elif sys.argv[1] == 'list':
             if len(sys.argv) <= 4:
                 exit(
                     "Please provide command arguments in this order: acc_connection_uri, container_name, sas_token")
             else:
                 list_blobs()
+        elif sys.argv[1] == 'download':
+            if len(sys.argv) <= 4:
+                exit(
+                    "Please provide command arguments in this order: acc_connection_uri, container_name, sas_token")
+            else:
+                download_blobs()
+
     else:
         print('\nError: Invalid command. Use "am.py --help" for available commands')
 
@@ -40,7 +49,6 @@ def decider():
 # Upload all blobs from a specified folder to a specified container
 def upload_all():
     # Initialize the connection to Azure storage account
-    blob_service_client = BlobServiceClient.from_connection_string(os.getenv('AZURE_CON_STR'))
     image_content_setting = ContentSettings(content_type='image/jpeg')
     # Go through all files in provided directory
     all_files = [f for f in os.listdir(sys.argv[3])]
@@ -58,6 +66,36 @@ def list_blobs():
 
     for blob in blobs:
         print(blob.name + '\n')
+
+
+# Download all blobs, or specified blob, to a folder
+def download_blobs():
+    download_dir = './downloads'
+    container_client = ContainerClient(sys.argv[2], sys.argv[3], sys.argv[4])
+    blobs = container_client.list_blobs()
+
+    # If we have 6 arguments, the user has specified a blob to download as last argument
+    if len(sys.argv) == 6:
+        # If download dir doesn't exist, make it first
+        if not os.path.exists(download_dir):
+            os.mkdir(download_dir)
+            with open(os.path.join(download_dir, sys.argv[5]), "wb") as file:
+                file.write(container_client.get_blob_client(sys.argv[5]).download_blob().readall())
+        else:
+            with open(os.path.join(download_dir, sys.argv[5]), "wb") as file:
+                file.write(container_client.get_blob_client(sys.argv[5]).download_blob().readall())
+    # Otherwise download all blobs in container
+    else:
+        if not os.path.exists(download_dir):
+            os.mkdir(download_dir)
+            for blob in blobs:
+                with open(os.path.join(download_dir, blob.name), "wb") as file:
+                    file.write(container_client.get_blob_client(blob.name).download_blob().readall())
+        else:
+            for blob in blobs:
+                with open(os.path.join(download_dir, blob.name), "wb") as file:
+                    file.write(container_client.get_blob_client(blob.name).download_blob().readall())
+    print(f"Successfully downloaded your specified files to the {download_dir} folder")
 
 
 if __name__ == "__main__":
--- src/helpers.py
+++ src/helpers.py
@@ -5,22 +5,16 @@ import sys
 parser = argparse.ArgumentParser \
     (description='Welcome to MS Azure Manager!\nThis program allows you to perform '
                  'several actions with your MS Azure storage account. See available '
-                 'options below: ', usage='am.py [-h] <options>')
+                 'options below:', usage="am.py [-h | --help] [upload | list | download | download]")
 
 
 def show_help():
-    upload_blobs_help = parser.add_argument_group('Option 1: "am.py upload <container_name> <path_to_folder>"',
-                                                  'Upload all blobs in '
-                                                  'folder to your chosen container')
-    upload_blobs_help.add_argument('container_name', help='Name of your container')
-    upload_blobs_help.add_argument('path_to_folder', help='Path to folder containing your files')
-
-    list_blobs_help = parser.add_argument_group('Option 2: "am.py showall <acc_connection_uri> <container_name> '
-                                                '<sas_token>"', 'Show all blobs currently stored in your chosen '
-                                                'container.')
-    list_blobs_help.add_argument('acc_connection_uri', help='Your Azure connection URI')
-    list_blobs_help.add_argument('container_name', help='Name of your container')
-    list_blobs_help.add_argument('sas_token', help='Your Azure SAS token')
+    parser.add_argument('upload', help="Batch upload blobs - am.py upload <container_name> <file_path>")
+    parser.add_argument('list', help="List all blobs - am.py list <acc_connection_uri> <container_name> <sas_token>")
+    parser.add_argument('download', help="Download all blobs - am.py download <acc_connection_uri> "
+                                         "<container_name> <sas_token>")
+    parser.add_argument('download', help="Download only one blob - am.py download <acc_connection_uri> "
+                                         "<container_name> <sas_token> <blob_name>")
 
     if len(sys.argv) == 2:
         parser.print_help()
